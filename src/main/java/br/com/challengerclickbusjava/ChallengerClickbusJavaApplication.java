33cdabd18ddb2877497ea723196eabf1a1731cd8
tiago <ti.almeida58@gmail.com>
2025-01-14 10:53:32 -0300

    feat: creating PlaceNotFoundException

---
+++ src/main/java/br/com/challengerclickbusjava/ChallengerClickbusJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.challengerclickbusjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerClickbusJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengerClickbusJavaApplication.class, args);
+    }
+
+}
